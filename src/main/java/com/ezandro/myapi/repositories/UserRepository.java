1abaada5815798765b4598a5a2f6d524e20042e6
Ezandro Bueno <ezandrobueno@yahoo.com.br>
2021-03-06 17:11:06 -0300

    Repository

--- src/main/java/com/ezandro/myapi/repositories/UserRepository.java
+++ src/main/java/com/ezandro/myapi/repositories/UserRepository.java
@@ -1,8 +1,10 @@
 package com.ezandro.myapi.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ezandro.myapi.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 }
