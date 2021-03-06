91b8978be668c2df1bf7788cafb5ada358140f2e
Ezandro Bueno <ezandrobueno@yahoo.com.br>
2021-03-06 17:20:27 -0300

    Insert data for testing

--- src/main/java/com/ezandro/myapi/MyapiApplication.java
+++ src/main/java/com/ezandro/myapi/MyapiApplication.java
@@ -1,13 +1,31 @@
 package com.ezandro.myapi;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ezandro.myapi.entities.User;
+import com.ezandro.myapi.repositories.UserRepository;
+
 @SpringBootApplication
-public class MyapiApplication {
+public class MyapiApplication implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyapiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		User u1 = new User(null, "Ezandro Bueno", "bueno", "123");
+		User u2 = new User(null, "Nayara Balarotti", "nay", "123");
+
+		userRepository.saveAll(Arrays.asList(u1, u2));
 	}
 
 }
