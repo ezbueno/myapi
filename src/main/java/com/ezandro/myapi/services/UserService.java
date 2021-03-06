26a093e49446d9f76f5c780c04f3fbcc61f0bba7
Ezandro Bueno <ezandrobueno@yahoo.com.br>
2021-03-06 18:22:11 -0300

    Service/findAll

--- src/main/java/com/ezandro/myapi/services/UserService.java
+++ src/main/java/com/ezandro/myapi/services/UserService.java
@@ -1,5 +1,6 @@
 package com.ezandro.myapi.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,11 @@ public class UserService {
 	public User findById(Integer id) {
 		Optional<User> user = userRepository.findById(id);
 		return user.orElseThrow(() -> new UserNotFoundException("Usuário não encontrado! Id: " + id + ", Tipo: " + User.class.getName()));
+	}
+	
+	public List<User> findAll() {
+		List<User> users = userRepository.findAll();
+		return users;
 	}
 
 }
