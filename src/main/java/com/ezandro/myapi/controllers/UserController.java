621ff41dc12b4e2ff6f1473d73545daea1a30b28
Ezandro Bueno <ezandrobueno@yahoo.com.br>
2021-03-06 18:40:48 -0300

    Controller/PUT

--- src/main/java/com/ezandro/myapi/controllers/UserController.java
+++ src/main/java/com/ezandro/myapi/controllers/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,5 +31,11 @@ public class UserController {
 	public ResponseEntity<List<User>> findAll() {
 		List<User> users = userService.findAll();
 		return ResponseEntity.ok().body(users);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Integer id, @RequestBody User user) {
+		User newUser = userService.update(id, user);
+		return ResponseEntity.ok().body(newUser);
 	}
 }
