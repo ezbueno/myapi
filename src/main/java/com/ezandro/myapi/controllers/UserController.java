15a0e14e0eafb0f1f75b93117b34743cf5a2435d
Ezandro Bueno <ezandrobueno@yahoo.com.br>
2021-03-06 22:22:15 -0300

    Controller/POST

--- src/main/java/com/ezandro/myapi/controllers/UserController.java
+++ src/main/java/com/ezandro/myapi/controllers/UserController.java
@@ -1,15 +1,18 @@
 package com.ezandro.myapi.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ezandro.myapi.entities.User;
 import com.ezandro.myapi.services.UserService;
@@ -37,5 +40,12 @@ public class UserController {
 	public ResponseEntity<User> update(@PathVariable Integer id, @RequestBody User user) {
 		User newUser = userService.update(id, user);
 		return ResponseEntity.ok().body(newUser);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> create(@RequestBody User user) {
+		User newUser = userService.create(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newUser.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
